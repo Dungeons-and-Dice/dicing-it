b0354fb9ac7af2f85087d0fb9931296ff1490a4b
LucasNemeth <l_orion_n@hotmail.com>
2020-10-08 13:48:07 -0400

    basic roll function

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+    let dice ={
+        side:6,
+        roll: function (){
+            const randomNumber = Math.floor(Math.random() * this.side) + 1;
+            return randomNumber
+        },
+        
+    }
+    
+    
+
+    $("#rollbutton").click(function(){
+            let result = dice.roll();
+            console.log(result)
+            $(function(){
+                $('#printedNumber').text(`${result}`)
+             })
+        })
+
+})
+    
+
+    
+        
