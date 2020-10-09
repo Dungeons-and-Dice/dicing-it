550cc24a56cc19ebcaddb110ac11486c98d73d69
Audra T <audraturuta@gmail.com>
2020-10-09 14:23:25 -0400

    Added second dice section, added test radio buttons for dice selection, currently getting value of radio buttons on submit

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 $(function(){
-    // let dice ={
-    //     side:6,
-    //     roll: function (){
-    //         const randomNumber = Math.floor(Math.random() * this.side) + 1;
-    //         return randomNumber
-    //     },
-        
-    // }
-
     let chosenDice = 0;
 
     let diceOptions = {
@@ -22,24 +13,30 @@ $(function(){
             const randomNumber = Math.floor(Math.random() * diceOptions[chosenDice]) + 1;
             return(randomNumber);
         },
-    }
-
-    //to get value of modifier, we need to add number to text box, .val and add it to the rolled number
-
+    };
 
     $('#buttons button').click(function() {
         chosenDice = $(this).attr('id');
         modifier = parseInt( $("#modifier").val());
-        console.log(modifier)
-        let diceRoll = diceOptions.roll()
-        console.log(diceRoll)
+        console.log(modifier);
+        let diceRoll = diceOptions.roll();
+        console.log(diceRoll);
         let result = diceRoll + modifier;
         
         $('#printedNumber').text(`${result}`);
+    });
 
+    // Second Dice Part
+
+    $('#otherDice button').click(function () {
+        let radioValue = $('input[name="dice"]:checked').val();
+        console.log(radioValue);
     });
 });
-    
 
+// Input field, get number, roll that many dice
+
+    
+// * Do we want to display the roll and modifier? 
     
         
